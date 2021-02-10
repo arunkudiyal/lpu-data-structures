80f4a54e22c202f69638a74088b558228d908c0b
Arun Kudiyal <37402172+arunkudiyal@users.noreply.github.com>
2021-02-10 20:44:27 +0530

    Code Updated

---
+++ DayThree.java
@@ -0,0 +1,128 @@
+package com.company;
+
+// Step 1
+import java.util.Scanner;
+
+public class DayThree {
+
+    public static void main(String[] args) {
+
+        // How to take input from the user
+
+        // Step 2: Create an instance of the Scanner class
+        // className instaceName = new className()
+        Scanner sc = new Scanner(System.in);
+
+        // take two integer inputs, add them and display them
+//        System.out.print("Enter the value of a: ");
+//        int a = sc.nextInt();
+//
+//        System.out.print("Enter the value of b: ");
+//        int b = sc.nextInt();
+//          int c = a + b;
+//
+//        System.out.println("The sum is: " + c);
+
+        // take firstName and lastName and print the full name
+//        System.out.print("Enter the First Name: ");
+//        String firstName = sc.next();
+//
+//        System.out.print("Enter the Last Name: ");
+//        String lastName = sc.next();
+//
+//        String fullName = firstName + " " + lastName;
+//        System.out.println("Your full name is: " + fullName);
+
+
+        // TYPE CASTING
+//        int a = 100;
+//        int b = 14;
+//
+//        // 100 / 14
+//        // Type casting
+//        double c = (double)a / (double)b;
+//
+//        // interting part
+//        System.out.println(a + "  " + b);
+//
+//        System.out.println("The division is: " + c);
+//
+//        System.out.print("Arun");
+//        System.out.println("Hello");
+//
+//        char ch = 'a';
+//        int value = ch;
+//        System.out.println("The value is: " + value);
+
+
+        // ARRAYS
+
+        // You want marks of 10 people at a same time
+        // 1. create 10 variables and ask your user to input
+
+        // creating one variable which is going to contain multiple values inside it
+        // MULTIPLE VALUES, ALL HAVE SAME DATATYPE
+
+        // Declare an array
+        // 1. dataType[] arrayName = { values seperated by the commas }
+
+        // Declared an array
+        String[] str;                     // Empty array
+        int[] values = {10, 20, 30, 40, 50};
+
+        // access the array = arrayName[index_you_want_to_access]
+        System.out.println("The value at index 0 is: " + values[0]);
+        System.out.println("The value at index 1 is: " + values[1]);
+        System.out.println("The value at index 2 is: " + values[2]);
+        System.out.println("The value at index 3 is: " + values[3]);
+        System.out.println("The value at index 4 is: " + values[4]);
+
+        // Insert a value to an array
+//        str[0] = "Arun";
+//        str[1] = "Amit";
+//        str[2] = "Sumit";
+//
+//        System.out.println(str[0]);
+//        System.out.println(str[1]);
+//        System.out.println(str[2]);
+
+        // 2. Decalraing an array
+        // -> create an empty array, where i can put fixed amount of data
+        // dataType[] arrayName = new dataType[size]
+
+        // declaring an array and initializing it by telling JVM to reserve
+        // some memory for particular size of array
+//        int[] arr = new int[10];
+//
+//        str = new String[5];
+//
+//        System.out.println(arr[0]);
+//
+//        System.out.println(str[0]);
+
+
+        // OPERATORS -> 2 + 3 -> Arithmetic Operators [ +, -, *, / ]
+
+        // 1. Logical Operator
+        // 2. Relational Operator
+        // 3. Conditional Operators
+
+        // 1. greater than
+        int a = 10;
+        int b = 20;
+
+        boolean check = a >= b;
+        System.out.println(check);
+
+        int[] arr = new int[10];
+        // [0 ... 9]
+        System.out.println(arr[10]);
+
+        // &&
+        // (condition1) && (condition2) { }
+        // (condition1) || (condition2) { }
+        // !(condition) { }
+
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,73 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("123");
+//        System.out.println("Arun");
+//
+//        // We can create variables -> Comment
+//        /* This is a
+//                multi-line comment
+//         */
+//
+//        // 12345, "Sarthak", true/false
+//
+//        // print("\n") = println()
+//
+//        // decalaring & initializing your variable at a single go
+//        int variable1 = 12345;
+//        System.out.println(variable1);
+//
+//        // declaring a variable
+//        int variable2;
+//        // initializing to the variable
+//        variable2 = 34567;
+
+        // Program for school management
+//        String studentName;
+//        int subject1, subject2, subject3, subject4, total;
+//
+//        studentName = "Tim";
+//        subject1 = 91;
+//        subject2 = 67;
+//        subject3 = 75;
+//        subject4 = 31;
+//
+//        total = subject1 + subject2 + subject3 + subject4;
+//
+//        // way 1
+//        // System.out.println("The total marks of " + studentName + " is: " + (subject1+subject2+subject3+subject4) + " / 400");
+//
+//        // way -2
+//        System.out.println("The total marks of " + studentName + " is: " + total + " / 400");
+
+
+//        int num1 = 10;
+//        int num2 = 5;
+//
+//        num1 = num2 + 5;        // num1 = 5 + 5 = 10
+//        num2 = num1 + 6;        // num2 = 10 + 6 = 16
+//
+//        float b = 0.0f;
+//        b = (float) (num1 * 0.01);
+//
+//        boolean isTrue = true;
+//
+//        // Print the square of 10 and cube of 5
+//        int squareOfTen = num1 * num1;
+//        System.out.println("The square of num1 is: " + sqaureeOfTen);
+//
+//        System.out.println("10% of num1 is: " + b);
+//
+//        System.out.println("The cube of num2 is: " + (num2*num2*num2));
+
+
+        // Take the input from the user [ C - scanf(), C++ - cin >> ]
+
+        // In order to take input from the user you have to takr help of an important Java class
+        // called as Scanner
+
+        // vvImp -> Scanner class is a part of packagae called java.util
+
+    }
+}
