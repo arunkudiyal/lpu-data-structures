0e33735b4063942299447e81e9a69c5056ce0c04
Arun Kudiyal <37402172+arunkudiyal@users.noreply.github.com>
2021-02-23 08:56:14 +0530

    Code Updated

---
+++ ArrayRepresentation.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ArrayRepresentation {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        // Ask user for the size of the array
+//        System.out.print("Enter the size of the array: ");
+//        int n = sc.nextInt();
+//
+//        // Create the array of the specified size
+//        int[] arr = new int[n];
+//
+//        // Ask the user for the elements in the array
+//        // arr[index] = value;
+//        System.out.println("Enter the values of the array!");
+//        for(int i=0; i < n; i++) {
+//            System.out.print("Enter the value of index " + i + " - ");
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        // Display the array
+//        System.out.println("\nThe elements in the array are:-");
+//        // arr[0], arr[1], arr[2] ... arr[n-1]
+//        for(int i=0; i<n; i++) {
+//            System.out.println("Value at index " + i + " is - " + arr[i]);
+//        }
+
+
+        // Q: Print even values from 0 to 3500
+//        for(int i=0; i<=3500; i++) {            // i = 0 to 3500
+//            // check each value of i
+//            if(i%2 != 0) {
+//                continue;
+//            }
+//            System.out.print(i + " ");
+//        }
+
+    }
+}
